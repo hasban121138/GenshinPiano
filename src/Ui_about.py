bde7a608b50c590e86c55e532b0d36181bf1498a
hasban12138 <1483478664@qq.com>
2021-04-03 17:51:35 +0800

    添加github地址

--- src/Ui_about.py
+++ src/Ui_about.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5 import QtCore, QtWidgets
+# Form implementation generated from reading ui file 'c:\Users\Administrator\Desktop\hasan\GenshinPiano\res\ui\about.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_About(object):
     def setupUi(self, About):
@@ -10,6 +15,7 @@ class Ui_About(object):
         self.label = QtWidgets.QLabel(About)
         self.label.setGeometry(QtCore.QRect(10, 10, 131, 141))
         self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../img/hutao.png"))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(About)
         self.label_2.setGeometry(QtCore.QRect(160, 10, 271, 141))
@@ -21,16 +27,12 @@ class Ui_About(object):
     def retranslateUi(self, About):
         _translate = QtCore.QCoreApplication.translate
         About.setWindowTitle(_translate("About", "Dialog"))
-        self.label_2.setText(
-            _translate(
-                "About",
-                "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                "p, li { white-space: pre-wrap; }\n"
-                "</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-                "<p align=\"center\" style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">关于自动弹奏工具</span></p>\n"
-                "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">版本:0.9b</span></p>\n"
-                "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">作者:</span><a href=\"https://space.bilibili.com/40358750\"><span style=\" font-size:14pt; text-decoration: underline; color:#0000ff;\">此号已封12138</span></a></p>\n"
-                "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\n"
-                "<span style=\" font-size:14pt; color:#000000;\">GitHub:</span></p></body></html>"
-            ))
+        self.label_2.setText(_translate("About", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt;\">关于自动弹奏工具</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">版本:0.9b</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">作者:</span><a href=\"https://space.bilibili.com/40358750\"><span style=\" font-size:14pt; text-decoration: underline; color:#0000ff;\">此号已封12138</span></a></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#000000;\">GitHub:</span><a href=\"https://github.com/hasban121138/GenshinPiano\"><span style=\" font-size:14pt; text-decoration: underline; color:#0000ff;\">GenshinPiano</span></a></p></body></html>"))
+
--- src/Ui_use.py
+++ src/Ui_use.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5 import QtCore, QtWidgets
+# Form implementation generated from reading ui file 'c:\Users\Administrator\Desktop\hasan\GenshinPiano\res\ui\use.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Use(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(548, 286)
+        Dialog.resize(546, 328)
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(10, 10, 461, 261))
+        self.label.setGeometry(QtCore.QRect(10, 0, 461, 321))
         self.label.setObjectName("label")
 
         self.retranslateUi(Dialog)
@@ -17,8 +22,5 @@ class Ui_Use(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(
-            _translate(
-                "Dialog",
-                "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">注意:</span></p><p><span style=\" font-size:14pt; font-weight:600;\">由于是按键模拟器,无法保证完全安全,请谨慎使用</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用说明:</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用管理员身份运行</span></p><p><span style=\" font-size:14pt; font-weight:600;\">在输入框中输入字符，点击开始即可弹奏</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用括号(英文)表示和弦</span></p><p><span style=\" font-size:14pt; font-weight:600;\">例如:(AE)表示同时按下AE</span></p><p><span style=\" font-size:14pt; font-weight:600;\">延迟为每个音符的停顿时长，单位:毫秒</span></p><p><span style=\" font-size:14pt; font-weight:600;\">请尽可能不要输入奇怪的东西防止出现Bug</span></p></body></html>"
-            ))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">注意:</span></p><p><span style=\" font-size:14pt; font-weight:600;\">由于是按键模拟器,无法保证完全安全,请谨慎使用</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用说明:</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用管理员身份运行</span></p><p><span style=\" font-size:14pt; font-weight:600;\">在输入框中输入字符，点击开始即可弹奏</span></p><p><span style=\" font-size:14pt; font-weight:600;\">使用括号(英文)表示和弦</span></p><p><span style=\" font-size:14pt; font-weight:600;\">例如:(AE)表示同时按下AE</span></p><p><span style=\" font-size:14pt; font-weight:600;\">延迟为每个音符的停顿时长，单位:毫秒</span></p><p><span style=\" font-size:14pt; font-weight:600;\">请尽可能不要输入奇怪的东西防止出现Bug</span></p></body></html>"))
+
